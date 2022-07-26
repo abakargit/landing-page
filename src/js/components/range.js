f0ae53c812cad9a4518262c1f53b6c06120b4b01
HubCrag <hi@hubcrag.com>
2022-07-26 15:33:21 +0300

    Init gh-pages

--- src/js/components/range.js
+++
@@ -1,7 +0,0 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("range-demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
